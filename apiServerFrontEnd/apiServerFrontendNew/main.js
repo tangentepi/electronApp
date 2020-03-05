d2f9af3f2b1b5bde2f7057808f222b090b4921ed
tengentepi <lohsandjoh@yahoo.fr>
2020-03-05 00:44:11 +0000

    Ajout du dossier des frontEnd renommage des dossiers back et front

---
+++ apiServerFrontEnd/apiServerFrontendNew/main.js
@@ -0,0 +1,29 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow, mainWindow1;
+
+function createWindow(){
+    mainWindow = new BrowserWindow({width:1000, height:750, movable: true, resizable: false, icon: './ico/add1.png'});
+    mainWindow.loadURL(`file://${__dirname}./views/handlePatientsPage.html`);
+    mainWindow.on(`closed`, () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.plateform !== 'darwin'){
+        app.quit();
+    }
+} );
+
+app.on('activate', () => {
+    if (mainWindow === null){
+        createWindow();
+    }
+});
+
+

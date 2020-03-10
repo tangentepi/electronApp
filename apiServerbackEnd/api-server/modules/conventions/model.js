a652d882c4f5f1f52a84aa4cfbc6ab3cc3c698fc
tengentepi <lohsandjoh@yahoo.fr>
2020-03-10 16:00:51 +0000

    Organisation des models

--- apiServerbackEnd/api-server/modules/conventions/model.js
+++ apiServerbackEnd/api-server/modules/conventions/model.js
@@ -4,7 +4,6 @@ const conventionSchema = new mongoose.Schema({
     wording: {type: String, required: true}, //Libellé de la convention
     membershipCost: {type: String, required: true}, //Coût d'adhésion à la convention
     insuredShare: {type: Number, required}, //Part de l'adhérent (en %)
-    patientIds: [String]
 });
 
 export default mongoose.model('Convention', conventionSchema);

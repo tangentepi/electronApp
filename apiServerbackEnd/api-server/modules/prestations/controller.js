8842f355d6755d7654f67b65ce62dc07f380bc95
tengentepi <lohsandjoh@yahoo.fr>
2020-03-11 14:42:44 +0000

    Organisation des fichiers

--- apiServerbackEnd/api-server/modules/prestations/controller.js
+++ apiServerbackEnd/api-server/modules/prestations/controller.js
@@ -5,4 +5,17 @@ export const createPrestation = (req, res, next) => {
         wording: req.body.wording,
         centerIds: req.body.centerId
     });
+    prestation.save().then(
+        () => {
+            res.status(201).json({
+                message: 'Prestation save successfully !'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error : error
+            });
+        }
+    );
 };
